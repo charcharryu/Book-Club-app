10fbb8eb2b81e6a9fbf732dc1fe8b0f9c9b88161
charcharryu <78817594+charcharryu@users.noreply.github.com>
2021-10-25 13:04:12 -0700

    Added comments about React.

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
  import React from 'react';
  import { render } from 'react-dom';
  import App from './components/App';
-
+// rendering the App component into our html file.
  render(
    <App />,
    document.getElementById('root')
